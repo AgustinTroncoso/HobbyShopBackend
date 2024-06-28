3e496594e831d5c4123067facd08fc8e1f30366d
AgustinTroncoso <a.troncoso09@ufromail.cl>
2024-06-28 18:48:28 -0400

    feat: create project structure and models
    Initialize project via Spring Boot. Create models for Reseña and Producto. Add related files such as Services, Controllers, and Repositories.

---
+++ demo/src/main/java/com/example/demo/models/Producto.java
@@ -0,0 +1,44 @@
+package com.example.demo.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name="producto")
+public class Producto {
+        @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Integer id;
+    
+    @Column(name="nombreproducto", length=8)
+    private String nombreproducto;
+
+    @Column(name="precio", length=30)
+    private Integer precio;
+
+    @Column(name="descripcion", length=500)
+    private String descripcion;
+
+    @Column(name="stock", length=30)
+    private Integer stock;
+
+    @Column(name="imageUrl", length=300)
+    private String imageUrl;
+
+
+}
---
+++ demo/src/main/java/com/example/demo/models/Reseña.java
@@ -0,0 +1,34 @@
+package com.example.demo.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name="reseña")
+public class Reseña {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+     @Column(name = "nombre", length = 20)
+    private String nombre;
+
+    @Column(name = "descripcion", length = 500)
+    private String descripcion;
+
+
+}
