8bc78b814ba09deb5e03b0fc9cb5de69c339d245
AgustinTroncoso <a.troncoso09@ufromail.cl>
2024-06-29 14:34:27 -0400

    feat: add basic endpoints for ProductoController and ReseñaController
    Implemented basic CRUD endpoints for ProductoController and ReseñaController.

--- demo/src/main/java/com/example/demo/controllers/ProductoController.java
+++ demo/src/main/java/com/example/demo/controllers/ProductoController.java
@@ -1,5 +1,68 @@
 package com.example.demo.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.models.Producto;
+import com.example.demo.services.ProductoServices;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/productos")
+@RequiredArgsConstructor
+
 public class ProductoController {
     
+
+    private final ProductoServices productoServices;
+
+        @GetMapping
+    public ResponseEntity<?> getAllProducto() throws Exception {
+        try {
+            return ResponseEntity.ok(productoServices.getAll());
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createProducto(@RequestBody Producto productoToCreate) throws Exception {
+        try {
+            return ResponseEntity.ok(productoServices.createProducto(productoToCreate));
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+  @DeleteMapping("/{productoID}")
+    public ResponseEntity<?> deleteProducto(@PathVariable Long productoID) throws Exception {
+        try {
+            productoServices.deleteProductoByID(productoID);
+            return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    
+    @PutMapping
+    public ResponseEntity<?> updateProducto(@RequestBody Producto bookToUpdate) throws Exception {
+        try {
+            return ResponseEntity.ok(productoServices.updateBook(bookToUpdate));
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+
+
 }
--- demo/src/main/java/com/example/demo/controllers/ReseñaController.java
+++ demo/src/main/java/com/example/demo/controllers/ReseñaController.java
@@ -1,5 +1,68 @@
 package com.example.demo.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.models.Reseña;
+import com.example.demo.services.ReseñaServices;
+
+import lombok.RequiredArgsConstructor;
+
+
+@RestController
+@RequestMapping("/api/reseña")
+@RequiredArgsConstructor
+
 public class ReseñaController {
+  
     
+    
+    private final ReseñaServices reseñaServices;
+
+        @GetMapping
+    public ResponseEntity<?> getAllReseña() throws Exception {
+        try {
+            return ResponseEntity.ok(reseñaServices.getAll());
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createReseña(@RequestBody Reseña reseñaToCreate) throws Exception {
+        try {
+            return ResponseEntity.ok(reseñaServices.createReseña(reseñaToCreate));
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+  @DeleteMapping("/{reseñaID}")
+    public ResponseEntity<?> deleteReseña(@PathVariable Long reseñaID) throws Exception {
+        try {
+            reseñaServices.deleteReseñaByID(reseñaID);
+            return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    
+    @PutMapping
+    public ResponseEntity<?> updateReseña(@RequestBody Reseña reseñaToUpdate) throws Exception {
+        try {
+            return ResponseEntity.ok(reseñaServices.updateReseña(reseñaToUpdate));
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
 }
