dbdcdfadd1b35610d33d25a20eb3f14792863395
AgustinTroncoso <a.troncoso09@ufromail.cl>
2024-07-01 21:41:58 -0400

    chore: add @CrossOrigin("*") to ProductoController and ReseñaController
    Added @CrossOrigin("*") to enable cross-origin resource sharing in ProductoController and ReseñaController.

--- demo/src/main/java/com/example/demo/controllers/ProductoController.java
+++ demo/src/main/java/com/example/demo/controllers/ProductoController.java
@@ -2,6 +2,7 @@ package com.example.demo.controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,7 +20,7 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("/api/productos")
 @RequiredArgsConstructor
-
+@CrossOrigin(origins = "*")
 public class ProductoController {
     
 
